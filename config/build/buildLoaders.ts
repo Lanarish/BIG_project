86a2c385fca663937e1b2f7e9d6aa8e31138ba59
Svetlana Malyugina <lanarish@gmail.com>
2023-05-02 17:25:48 +0400

    Add cust comp Btn to switch themes, add svg loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,35 +4,52 @@ import { BuildOptions } from "./types/config";
 
 export function buildLoaders({isDev}:BuildOptions): webpack.RuleSetRule[] {
 
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  }
+  
+  const cssLoader = 
+    {
+      test: /\.s[ac]ss$/i,
+      use: [
+        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+        
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: (resPath: string) => Boolean(resPath.includes('module.')),
+            localIdentName: isDev
+             ? '[path][name]__[local]' 
+             : '[hash:base64:5]',
+                    },
+            },
+            
+      },
+        "sass-loader",
+      ],
+    };
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
         };
-        
-    const cssLoader = 
+
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-          
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                auto: (resPath: string) => Boolean(resPath.includes('module.')),
-              localIdentName: isDev
-               ? '[path][name]__[local]' 
-               : '[hash:base64:5]',
-                      },
-              },
-              
-        },
-          "sass-loader",
-        ],
-      };
+        loader: 'file-loader',
+      },
+    ],
+  }
+
       
   return [
+    fileLoader,
+    svgLoader,
     typescriptLoader,
     cssLoader
   ];
